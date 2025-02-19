2c74f986430f47b657b9a9438799fc5ffe59ed40
aa-nadim <nadim.ice.nstu@gmail.com>
2025-02-20 01:49:40 +0600

    updated

---
+++ 3.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# Load environment variables from .env
+load_dotenv()
+
+# Create a ChatOpenAI model
+model = ChatGoogleGenerativeAI(model="gemini-pro")
+
+
+chat_history = []  # Use a list to store messages
+
+# Set an initial system message (optional)
+
+
+
+# Chat loop
+while True:
+    query = input("You: ")
+    if query.lower() == "exit":
+        break
+    chat_history.append(HumanMessage(content=query))  # Add user message
+
+    # Get AI response using history
+    result = model.invoke(chat_history)
+    response = result.content
+    chat_history.append(AIMessage(content=response))  # Add AI message
+
+    print(f"AI: {response}")
+
+
+print("---- Message History ----")
+print(chat_history)
